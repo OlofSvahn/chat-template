84a735def099cbcaabcbc00d8b1e32bf4469a121
SMAB\olsv <olof.svahn@wgsa>
2021-12-07 10:52:07 +0100

    Added focus in and out to text field

---
+++ app/components/chat/input.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class ChatInputComponent extends Component {
+    @tracked isCollapsed = false;
+
+    @action
+    showCollapse(){
+        $('.collapse').collapse("show");
+        this.isCollapsed = false;
+        console.log(this.isCollapsed);
+    }
+    @action
+    hideCollapse(){
+        $('.collapse').collapse("hide");
+        this.isCollapsed = true;
+        console.log(this.isCollapsed);
+        }
+}
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import { messages } from '../data/messages';
 
 export default class ChatRoute extends Route {
-    model(){
-        return messages;
-    }
+  model() {
+    return messages;
+  }
 }
